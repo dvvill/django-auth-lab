3b703ef1b696c882008dad65134415be9f62c204
dvvill <91037458+dvvill@users.noreply.github.com>
2021-12-14 20:05:13 -0500

    add sign up view

--- user/views/user.py
+++ user/views/user.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status, generics
 
-# Create your views here.
+class SignUp(generics.CreateAPIView):
+    # Override the authentication/permissions classes so this endpoint
+    # is not authenticated & we don't need any permissions to access it.
+    authentication_classes = ()
+    permission_classes = ()
+    
+    def post(self, request):
+         print('wahooo')
+         return Response({})
