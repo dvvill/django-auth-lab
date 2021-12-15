f1ecc69bb8e1e9ed2820c6f5e87a367fc14406ec
dvvill <91037458+dvvill@users.noreply.github.com>
2021-12-14 19:02:16 -0500

    added restframework to settings.py

--- django_auth_lab/settings.py
+++ django_auth_lab/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'user'
+    'user',
+    'rest_framework.authtoken'
 ]
 
 MIDDLEWARE = [
@@ -79,6 +80,15 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'django_auth_lab',
     }
+}
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication'
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated'
+    ]
 }
 
 
